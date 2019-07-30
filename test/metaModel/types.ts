242ef6d919593ef7256dbce497835a4ec8647f08
Tobias Laundal <tobias.laundal@ardoq.com>
2019-07-30 12:18:39 +0200

    A new beginning

--- test/metaModel/types.ts
+++
@@ -1,9 +0,0 @@
-export type MetaModelId = string;
-
-export interface MetaModelDefinition {
-  name: string;
-}
-
-export interface PersistedMetaModel extends MetaModelDefinition {
-  _id: MetaModelId;
-}
