371ae0ae2a04961c0fcb909e48c735bdb30f7252
Tobias Laundal <tobias.laundal@ardoq.com>
2019-09-18 21:53:46 +0200

    Type the action stream in globalActions

--- examples/globalActions.ts
+++ examples/globalActions.ts
@@ -1,11 +1,11 @@
 import { Subject } from 'rxjs';
 import { Action } from 'stream-patterns/types/Action';
-import { ActionDispatcher } from 'stream-patterns/types/helpers';
+import { ActionDispatcher, ActionStream } from 'stream-patterns/types/helpers';
 import { tag } from 'rxjs-spy/operators';
 
 const actionSubject$ = new Subject<Action<any>>();
 
-export const action$ = actionSubject$.pipe(tag('action$'));
+export const action$: ActionStream = actionSubject$.pipe(tag('action$'));
 
 export const dispatchAction: ActionDispatcher = action =>
   actionSubject$.next(action);
