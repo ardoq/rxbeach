65a34aabec89805c943029a76c9108ac81a25de1
Anton Abilov <anton_abilov@hotmail.com>
2019-09-30 12:17:00 +0200

    Updated publishing scripts - Removed copying of package.json to each submodule (isn't necessary) - Added rimraf to clear the dist/ folder before building - Added internals as an entrypoint to the package

--- prep-package.js
+++ prep-package.js
@@ -1,11 +1,8 @@
 const fs = require('fs');
 const path = require('path');
-const packageJson = require('./package.json');
 const tsBuildConfig = require('./tsconfig-build.json');
 
-const NAME = packageJson.name;
 const TARGET = tsBuildConfig.compilerOptions.outDir;
-const SUBMODULES = ['internal', 'operators'];
 
 // Copy package.json from root dir to target, while removing the scripts key and
 // adding main and typings
@@ -17,16 +14,4 @@ fs.writeFileSync(
     typings: './index.d.ts',
     scripts: {},
   }, null, 2)
-);
-
-// Add minimal package.json files for the submodules
-SUBMODULES.forEach(name =>
-  fs.writeFileSync(
-    path.join(TARGET, name, 'package.json'),
-    JSON.stringify({
-      name: NAME + '/' + name,
-      main: 'index.js',
-      typings: './index.d.ts',
-    }, null, 2)
-  )
 );
