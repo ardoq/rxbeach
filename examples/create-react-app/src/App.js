69ef3e165467ce6e95285537db3fcb80bc43839d
Tobias Laundal <tobias.laundal@ardoq.com>
2019-10-08 13:08:33 +0200

    [create-react-app] Use rxbeach@0.0.4

--- examples/create-react-app/src/App.js
+++ examples/create-react-app/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { dispatchAction, action$ } from './globalActions';
 import { actionCreator } from 'rxbeach';
 import { ofType } from 'rxbeach/operators';
-import { scan, tap } from 'rxjs/operators';
+import { scan, tap } from 'rxjs/operators';
 import { useStream } from './utils';
 import { create } from 'rxjs-spy';
 
@@ -12,9 +12,9 @@ const increment = actionCreator('increment');
 spy.log(/action/);
 
 const incrementStream = action$.pipe(
-  ofType(increment.type),
+  ofType(increment),
   tap(() => console.log('incrementing')),
-  scan((acc) => acc + 1, 0)
+  scan(acc => acc + 1, 0)
 );
 
 function App() {
@@ -23,8 +23,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Value: {value}</h1>
-        <p>Open the console to see logging from the action stream</p> 
-        <button type="text" onClick={() => dispatchAction(increment())}>Increment</button> 
+        <p>Open the console to see logging from the action stream</p>
+        <button type="text" onClick={() => dispatchAction(increment())}>
+          Increment
+        </button>
       </header>
     </div>
   );
