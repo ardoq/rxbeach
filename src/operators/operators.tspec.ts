43a2f36d83fc125c3d20939dddfd14068d770c80
Tobias Laundal <tobias.laundal@ardoq.com>
2019-12-17 09:33:28 +0100

    Allow for actions with union payloads in ofType

---
+++ src/operators/operators.tspec.ts
@@ -0,0 +1,52 @@
+import { routine, actionCreator, Routine } from 'rxbeach';
+import { extractPayload, ofType } from 'rxbeach/operators';
+import { AssertTrue, Has } from 'conditional-type-checks';
+
+const stringRoutine = routine(
+  ofType(actionCreator<string>('simple string')),
+  extractPayload()
+);
+type StringRoutine = AssertTrue<Has<typeof stringRoutine, Routine<string>>>;
+
+type Overlap = { b: boolean };
+type Left = { a: number } & Overlap;
+type Right = Overlap & { c: symbol };
+const inferredOverlapRoutine = routine(
+  ofType(actionCreator<Overlap>('overlap'), actionCreator<Right>('right')),
+  extractPayload()
+);
+type InferredOverlapRoutine = AssertTrue<
+  Has<typeof inferredOverlapRoutine, Routine<Overlap>>
+>;
+
+const notInferredOverlapRoutine = routine(
+  ofType(actionCreator<Left>('left'), actionCreator<Right>('right')),
+  extractPayload()
+);
+type NotInferredOverlapRoutine = AssertTrue<
+  Has<typeof notInferredOverlapRoutine, Routine<{}>>
+>;
+
+const hintedInferredOverlapRoutine = routine(
+  ofType<Overlap>(actionCreator<Left>('left'), actionCreator<Right>('right')),
+  extractPayload()
+);
+type HintedInferredOverlapRoutine = AssertTrue<
+  Has<typeof hintedInferredOverlapRoutine, Routine<Overlap>>
+>;
+
+const unionRoutine = routine(
+  ofType(actionCreator<string | number>('string or number')),
+  extractPayload()
+);
+type UnionRoutine = AssertTrue<
+  Has<typeof unionRoutine, Routine<string | number>>
+>;
+
+const optionalPayloadRoutine = routine(
+  ofType(actionCreator<string | void>('optional string')),
+  extractPayload()
+);
+type OptionalPayloadRoutine = AssertTrue<
+  Has<typeof optionalPayloadRoutine, Routine<string | void>>
+>;
