c20c381f109b40280934aa9b88458c2f6cd3b505
Tobias Laundal <tobias.laundal@ardoq.com>
2020-03-19 14:38:16 +0100

    fixup! feat(react): add useState React hook

--- src/react/useStream.spec.ts
+++ src/react/useStream.spec.ts
@@ -1,20 +1,19 @@
 import test from 'ava';
 import { renderHook } from '@testing-library/react-hooks';
-import { useStream } from './useStream';
+import { useStream, NOT_YET_EMITTED } from './useStream';
 import { empty, Subject } from 'rxjs';
 
-const initial = 'x';
 const second = 'A';
 
 test('useStream return initial value right away', t => {
-  const { result } = renderHook(() => useStream(empty(), initial));
+  const { result } = renderHook(() => useStream(empty()));
 
-  t.deepEqual(result.current, 'x');
+  t.deepEqual(result.current, NOT_YET_EMITTED);
 });
 
 test('useStream return value from stream', t => {
   const source$ = new Subject<string>();
-  const { result } = renderHook(() => useStream(source$, initial));
+  const { result } = renderHook(() => useStream(source$));
 
   source$.next(second);
 
@@ -23,7 +22,7 @@ test('useStream return value from stream', t => {
 
 test('useStream unsubscribes on unmount', t => {
   const source$ = new Subject<string>();
-  const { unmount } = renderHook(() => useStream(source$, initial));
+  const { unmount } = renderHook(() => useStream(source$));
 
   unmount();
 
@@ -32,7 +31,7 @@ test('useStream unsubscribes on unmount', t => {
 
 test('useStream does not resubscribe on rerender', t => {
   const source$ = new Subject<string>();
-  const { rerender } = renderHook(() => useStream(source$, initial));
+  const { rerender } = renderHook(() => useStream(source$));
 
   const subscription = source$.observers[0];
   rerender();
@@ -40,36 +39,18 @@ test('useStream does not resubscribe on rerender', t => {
   t.assert(source$.observers[0] === subscription);
 });
 
-test('useStream does not update for changed initial value', t => {
-  const source$ = new Subject<string>();
-  const { rerender, result } = renderHook(
-    props => useStream(source$, props.initial),
-    {
-      initialProps: { initial },
-    }
-  );
-
-  source$.next(second);
-  rerender({ initial: 'something' });
-
-  t.deepEqual(result.current, second);
-});
-
 test('useStream unsubscribes, keeps latest value and subscribes new stream', t => {
-  const source1$ = new Subject<string>();
-  const source2$ = new Subject<string>();
+  const alpha = new Subject<string>();
+  const bravo = new Subject<string>();
 
-  const { rerender, result } = renderHook(
-    props => useStream(props.source$, initial),
-    {
-      initialProps: { source$: source1$ },
-    }
-  );
+  const { rerender, result } = renderHook(({ source$ }) => useStream(source$), {
+    initialProps: { source$: alpha },
+  });
 
-  source1$.next(second);
-  rerender({ source$: source2$ });
+  alpha.next(second);
+  rerender({ source$: bravo });
 
   t.deepEqual(result.current, second);
-  t.deepEqual(source1$.observers, []);
-  t.deepEqual(source2$.observers.length, 1);
+  t.deepEqual(alpha.observers, []);
+  t.deepEqual(bravo.observers.length, 1);
 });
--- src/react/useStream.ts
+++ src/react/useStream.ts
@@ -2,11 +2,28 @@ import { useState, useEffect } from 'react';
 import { Observable } from 'rxjs';
 
 /**
+ * Returned from `useStream` before first emit from the provided stream
+ */
+export const NOT_YET_EMITTED = Symbol('Returned from rxbeach/react:useStream');
+export type NOT_YET_EMITTED = typeof NOT_YET_EMITTED;
+
+/**
  * React hook to subscribe to a stream
  *
  * Each emit from the stream will make the component re-render with the new
- * value. Initially the `initial` argument is returned, because an `Observable`
+ * value. Initially, `NOT_YET_EMITTED` is returned, because an `Observable`
  * has no guarantee for when the first emit will happen.
+ *
+ * If you know the stream will emit immediately upon subscription, you can
+ * short-circuit the `NOT_YET_EMITTED` value to make your view return null:
+ * ```tsx
+ * const Component = () => {
+ *   const value = useStream(myStream$);
+ *   if (value === NOT_YET_EMITTED) return null;
+ *
+ *   ...
+ * };
+ * ```
  *
  * This hook passes the `source$` argument as a dependency to `useEffect`, which
  * means you will need to take care that it is referentially equal between each
@@ -20,18 +37,18 @@ import { Observable } from 'rxjs';
  *     ofType(action),
  *     extractPayload()
  *   ), [action]);
- *   const payload = useStream(payload$, null);
+ *   const payload = useStream(payload$);
  *
  *   return <p> Latest payload: { payload } </p>;
  * }
  * ```
  *
  * @param source$ Stream that provides the needed values
- * @param initial Initial value to return
  * @see useEffect
+ * @see NOT_YET_EMITTED
  */
-export const useStream = <T, I>(source$: Observable<T>, initial: I): T | I => {
-  const [value, setValue] = useState<T | I>(initial);
+export const useStream = <T>(source$: Observable<T>): T | NOT_YET_EMITTED => {
+  const [value, setValue] = useState<T | NOT_YET_EMITTED>(NOT_YET_EMITTED);
 
   useEffect(() => {
     const subscription = source$.subscribe(setValue);
