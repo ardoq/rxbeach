cbc1287f26b5411dd66d1ed7ef27545b808fc469
Sindre Seppola <sindre.seppola@gmail.com>
2023-02-23 13:12:37 +0100

    feat: action type helpers

--- src/types/helpers.ts
+++ src/types/helpers.ts
@@ -1,6 +1,10 @@
 import { Observable } from 'rxjs';
-import { ActionCreatorWithPayload } from './ActionCreator';
+import {
+  ActionCreatorWithoutPayload,
+  ActionCreatorWithPayload,
+} from './ActionCreator';
 import { UnknownAction } from '../internal/types';
+import { ActionWithPayload } from './Action';
 
 export type ActionStream = Observable<UnknownAction>;
 
@@ -18,3 +22,27 @@ export type ActionDispatcher = (
  */
 export type ExtractPayload<ActionType extends ActionCreatorWithPayload<any>> =
   ReturnType<ActionType>['payload'];
+
+/**
+ * Assert that an action creator is of a specific type, and extract its payload
+ * type
+ */
+export function isActionOfType<T = undefined>(
+  creatorFn: ActionCreatorWithPayload<T> | ActionCreatorWithoutPayload,
+  action: ActionWithPayload<unknown>
+): action is ActionWithPayload<T> {
+  const hasPayloadKey = 'payload' in action;
+  const hasMetaKey = 'meta' in action;
+
+  return action.type === creatorFn.type && hasPayloadKey && hasMetaKey;
+}
+
+/**
+ * Type helper to infer the payload of an action creator.
+ * This should be preferred over exporting the types separately.
+ */
+export type PayloadFromActionCreator<Fn> = Fn extends ActionCreatorWithPayload<
+  infer Payload
+>
+  ? Payload
+  : never;
